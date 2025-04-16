135be52e3e3596d03f3b93bfab1dbcc98f815f08
bertrand.gallice <bertrandgllc@gmail.com>
2025-04-16 19:30:54 +0200

    updated dependencies

--- src/crispy_kedro/pipelines/trisk/nodes/revenue_trajectories.py
+++ src/crispy_kedro/pipelines/trisk/nodes/revenue_trajectories.py
@@ -131,21 +131,21 @@ def filter_companies(
 
     # TODO : APPLY OWNERSHIP TREE ACCORDING TO EVENTS // ownership of events is ignored quick&dirty
     # Step 1: aggregate ownership by asset and company
-    companies_ownership_tree = plant_ownership_df.groupby(
-        ["asset_id", "owner_name", "company_id"], as_index=False
-    ).agg({"ownership_percentage": "sum"})
-    companies_ownership_tree["ownership_percentage"] = companies_ownership_tree[
-        "ownership_percentage"
-    ].astype(float)
+    # companies_ownership_tree = plant_ownership_df.groupby(
+    #     ["asset_id", "owner_name", "company_id"], as_index=False
+    # ).agg({"ownership_percentage": "sum"})
+    # companies_ownership_tree["ownership_percentage"] = companies_ownership_tree[
+    #     "ownership_percentage"
+    # ].astype(float)
 
-    # Step 2: normalize ownership per asset
-    companies_ownership_tree["normalized_ownership"] = companies_ownership_tree[
-        "ownership_percentage"
-    ] / companies_ownership_tree.groupby("asset_id")["ownership_percentage"].transform(
-        "sum"
-    )
+    # # Step 2: normalize ownership per asset
+    # companies_ownership_tree["normalized_ownership"] = companies_ownership_tree[
+    #     "ownership_percentage"
+    # ] / companies_ownership_tree.groupby("asset_id")["ownership_percentage"].transform(
+    #     "sum"
+    # )
 
-    return companies_ownership_tree[
+    return plant_ownership_df[
         ["asset_id", "owner_name", "company_id", "normalized_ownership"]
     ]
 
